b3d2c098a0920f460b466571abb6e8e729f9bc56
ctatienanh <minion4578@gmail.com>
2022-07-09 12:17:04 +0700

    lan 2 --- add modal

--- src/main/java/Modal/Film.java
+++ src/main/java/Modal/Film.java
@@ -29,6 +29,8 @@ public class Film {
 
     private int giaPhim;
 
+    private double star;
+
     public Film(int idPhim, String tenPhim, String thoiGian, String loaiPhim, String daoDien, String quocGia, String dienVien, String nhaSX, Date ngayKhoiChieu, String moTa, String trailer,String img,int giaPhim) {
         this.idPhim = idPhim;
         this.tenPhim = tenPhim;
@@ -44,6 +46,28 @@ public class Film {
         this.img=img;
         this.giaPhim=giaPhim;
     }
+
+
+    public Film(int idPhim, String tenPhim, String thoiGian, String loaiPhim, String daoDien, String quocGia, String dienVien, String nhaSX, Date ngayKhoiChieu, String moTa, String trailer, String img, double star) {
+        this.idPhim = idPhim;
+        this.tenPhim = tenPhim;
+        this.thoiGian = thoiGian;
+        this.loaiPhim = loaiPhim;
+        this.daoDien = daoDien;
+        this.quocGia = quocGia;
+        this.dienVien = dienVien;
+        this.nhaSX = nhaSX;
+        this.ngayKhoiChieu = ngayKhoiChieu;
+        this.moTa = moTa;
+        this.trailer = trailer;
+        this.img = img;
+        this.star = star;
+    }
+
+    public double getStar() {
+        return star;
+    }
+
 
     public int getGiaPhim() {
         return giaPhim;
